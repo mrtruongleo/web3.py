f30f6e9a94f86d14351d003441e28db8cc56420b
mrtruongleo <66201727+mrtruongleo@users.noreply.github.com>
2023-01-17 12:54:10 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -82,8 +82,8 @@ setup(
         "aiohttp>=3.7.4.post0",
         "eth-abi>=4.0.0-b.2",
         "parsimonious==0.9.0",  # TODO - fix in eth-abi
-        "eth-account-x @ git+https://github.com/mrtruongleo/eth-account.git",
-        "eth-hash-x @ git+https://github.com/mrtruongleo/eth-hash.git",
+        "eth-account @ git+https://github.com/mrtruongleo/eth-account.git",
+        "eth-hash @ git+https://github.com/mrtruongleo/eth-hash.git",
         "eth-typing>=3.0.0",
         "eth-utils>=2.1.0",
         "hexbytes>=0.1.0",
